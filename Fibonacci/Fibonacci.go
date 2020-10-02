a0b06e7cb04b4c8675ab51ea11a4dd0354008987
Shirish Amatya <47160465+samatya098@users.noreply.github.com>
2020-10-01 23:07:52 -0400

    Update Fibonacci.go

--- Fibonacci/Fibonacci.go
+++ Fibonacci/Fibonacci.go
@@ -1,1 +1,26 @@
+/*---------------------------------------------------------------------
+Shirish Amatya'
+10/01/2020
+This Program iterates the Fibonacci Sequence for the first 100 numbers
+using a recursive function
+---------------------------------------------------------------------*/
+package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func FibonacciRec(num int) int {
+	if num <= 1 {
+		return num
+	}
+	return FibonacciRec(num-1) + FibonacciRec(num-2)
+}
+
+func main() {
+	for i := 0; i <= 100; i++ {
+		fmt.Print(strconv.Itoa(FibonacciRec(i)) + " ")
+	}
+	fmt.Println("")
+}
